94046d2491b5f6e66a2f592dd73dec3c415bf658
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-02-16 05:15:01 +0530

    Add dashboard endpoint

--- capstone/app.py
+++ capstone/app.py
@@ -1,11 +1,12 @@
 from functools import wraps
 
 from flask import Flask, abort, request
-from kutty import html
-from kutty.bootstrap import Layout, Page, Card
+from kutty import html, Markdown
+from kutty.bootstrap import Layout, Page, Card, Hero
+from kutty.bootstrap.card import CardBody, CardText, CardTitle
 
 from .api import api
-from .db import Project, User
+from .db import Activity, Project, User
 
 
 app = Flask(__name__)
@@ -47,13 +48,54 @@ def make_project_card(title, short_description, tags, *extra):
     return card
 
 
+green_tick_mark = html.span(
+        html.HTML("""<svg style="height: 100%;" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M243.8 339.8C232.9 350.7 215.1 350.7 204.2 339.8L140.2 275.8C129.3 264.9 129.3 247.1 140.2 236.2C151.1 225.3 168.9 225.3 179.8 236.2L224 280.4L332.2 172.2C343.1 161.3 360.9 161.3 371.8 172.2C382.7 183.1 382.7 200.9 371.8 211.8L243.8 339.8zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>"""),
+        style="color: #28a745;", class_="px-2 mr-2")
+red_x_mark = html.span(
+        html.HTML("""<svg style="height: 100%;" fill="currentColor"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>"""),
+        style="color: #dc3545;", class_="px-2 mr-2")
+yellow_circle_mark = html.span(
+        html.HTML("""<svg style="height: 100%;" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>"""),
+        style="color: #ffc107;", class_="px-2 mr-2")
+
+
+def make_task_card(position, title, text, status=None):
+    status_marks = {
+        "Completed": green_tick_mark,
+        "Failing": red_x_mark,
+        "In Progress": yellow_circle_mark,
+    }
+    status_mark = status_marks.get(status, "")
+    collapsible_id = f"task-collapse-{position}"
+    card = Card(
+        CardBody(
+            CardTitle(f"{position}. {title}").add_class("mb-0"),
+            status_mark
+        ).add_class("d-flex justify-content-between"),
+        CardBody(
+            CardText(text),
+            class_="collapse py-0", id=collapsible_id,
+        )
+    ).add_class("my-3")
+    return html.a(card,
+                  class_="text-decoration-none text-reset",
+                  href=f"#{collapsible_id}",
+                  **{"data-toggle": "collapse"})
+
+
 def authenticated(handler):
     @wraps(handler)
     def wrapper(*args, **kwargs):
         # TODO: implement this
-        user = User.find(username="test")
+        user = get_authenticated_user(request)
+        if not user:
+            return abort(404)
         return handler(*args, user=user, **kwargs)
     return wrapper
+
+
+def get_authenticated_user(request):
+    return User.find(username="test")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -137,7 +179,7 @@ def dashboard(user):
 
         continue_button = html.div(
             html.button("Continue ›", class_="btn btn-secondary"),
-            class_="d-flex justify-content-end mt-2")
+            class_="d-flex justify-content-end mt-3")
         if user.has_started_project(project.name):
             card.body.add(continue_button)
             your_projects.add_column(card)
@@ -145,8 +187,46 @@ def dashboard(user):
             explore.add_column(card)
 
     if not your_projects.is_empty():
-        page << html.div(class_="mt-2").add(html.h2("Your Projects"), your_projects)
+        page << html.div(class_="mt-3").add(html.h3("Your Projects"), your_projects)
     if not explore.is_empty():
-        page << html.div(class_="mt-2").add(html.h2("Explore"), explore)
+        page << html.div(class_="mt-3").add(html.h3("Explore"), explore)
+
+    return layout.render_page(page)
+
+
+@app.route("/projects/<name>")
+@authenticated
+def project(name, user):
+    project = Project.find(name=name)
+    page = html.div()
+    hero = Hero(
+        title=project.title,
+        subtitle=project.short_description,
+        text=Markdown(project.description),
+    )
+    main = Page("")
+    page << hero
+    page << main
+
+    if user.has_started_project(project.name):
+        activity = Activity.find(
+            username=user.username, project_name=project.name)
+
+        task_activities_list = activity.get_tasks()
+        task_activities = {ta.task_id: ta for ta in task_activities_list}
+        for task in project.get_tasks():
+            task_activity = task_activities.get(task.id)
+            card = make_task_card(
+                position=task.position,
+                title=task.title,
+                text=Markdown(task.description),
+                status=task_activity and task_activity.status)
+            main << card
+    else:
+        hero.add_cta("Start Project", href="#")
+        for task in Project.find(name="build-your-own-shell").get_tasks():
+            card = make_task_card(
+                position=task.position, title=task.title, text=Markdown(task.description))
+            main << card
 
     return layout.render_page(page)
