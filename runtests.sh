64a0814a99a8f5567052b5fe9c86b218134d75b3
Anand Chitipothu <anandology@gmail.com>
2023-03-08 12:10:29 +0530

    Setup tests

---
+++ runtests.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [ -z "$TEST_DATABASE_URL" ]
+then
+	dropdb capstone_test --if-exists
+	createdb capstone_test
+	export TEST_DATABASE_URL=postgres:///capstone_test
+fi
+
+export DATABASE_URL=$TEST_DATABASE_URL
+
+python run.py --migrate
+
+export PYTHONPATH=.
+pytest "$@"
