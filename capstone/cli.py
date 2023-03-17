02bae4a6c51272ad744c3f2228f1b85ac4b9ea91
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-03-17 11:59:58 +0530

    cli: Add functionality to list, show, and delete users

--- capstone/cli.py
+++ capstone/cli.py
@@ -23,7 +23,22 @@ def make_table(rows, **kwargs):
         tablefmt="grid",
         maxcolwidths=12)
     options.update(kwargs)
-    return tabulate(rows, **options)
+    return tabulate(sanitize_rows(rows), **options)
+
+def sanitize_rows(rows):
+    """Convert None values to str(None).
+
+    Rows could be list, dict, dict_keys, or dict_values.
+    """
+    if isinstance(rows, list):
+        return [sanitize_rows(row) for row in rows]
+    elif isinstance(rows, dict):
+        return {k: sanitize_rows(v) for k, v in rows.items()}
+    elif rows is None:
+        return "None"
+    else:
+        return rows
+
 
 ## SITES
 
@@ -51,7 +66,7 @@ def sites_show(site):
     else:
         site_dict = db_site.get_dict()
         print(make_table(
-            {"key": site_dict.keys(), "value": site_dict.values()},
+            {"key": list(site_dict.keys()), "value": list(site_dict.values())},
             maxcolwidths=50))
 
 ## PROJECTS
@@ -92,7 +107,7 @@ def projects_show(site, project):
     project_dict = db_project.get_dict()
     project_dict["num_tasks"] = len(tasks)
     print(make_table(
-        {"key": project_dict.keys(), "value": project_dict.values()},
+        {"key": list(project_dict.keys()), "value": list(project_dict.values())},
         maxcolwidths=60))
     if tasks:
         # TODO: maybe figure out a better way to print checks?
@@ -124,6 +139,62 @@ def projects_delete(site, project):
     db_project.delete()
     print(f"Deleted project {project_name}")
 
+## USERS
+
+@cli.group()
+def users():
+    """manage projects"""
+    pass
+
+@users.command("list")
+@site_option()
+def users_list(site):
+    """Lists users of a site."""
+    print("List users", site)
+    db_site = db.Site.find(name=site)
+    if db_site is None:
+        print("Site not found")
+        sys.exit(1)
+    users = db.User.find_all(site_id=db_site.id)
+    print(make_table([user.get_teaser() for user in users]))
+
+@users.command("show")
+@site_option()
+@click.argument("username")
+def users_show(site, username):
+    """Show a user in a site."""
+    print("Show user", site, username)
+    db_site = db.Site.find(name=site)
+    if db_site is None:
+        print("Site not found")
+        sys.exit(1)
+    db_user = db.User.find(username=username)
+    if db_user is None:
+        print("User not found")
+        sys.exit(1)
+    user_dict = db_user.get_dict()
+    print(make_table(
+        {"key": list(user_dict.keys()),
+         "value": list(user_dict.values())},
+        maxcolwidths=60))
+
+@users.command("delete")
+@site_option()
+@click.argument("username")
+def users_delete(site, username):
+    """Delete a user in a site."""
+    print("Delete user", site, username)
+    db_site = db.Site.find(name=site)
+    if db_site is None:
+        print("Site not found")
+        sys.exit(1)
+    db_user = db.User.find(username=username)
+    if db_user is None:
+        print("User not found")
+        sys.exit(1)
+    user_name = db_user.username
+    db_user.delete()
+    print(f"Deleted user {user_name}")
 
 if __name__ == "__main__":
     cli()
