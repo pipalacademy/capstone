e78b899d19c9c4dd771b67d730b8b21afce6be98
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-03-30 19:47:14 +0530

    Fix #27: Use yaml.dump instead of tabulate to prettify CLI output

--- capstone/cli.py
+++ capstone/cli.py
@@ -1,6 +1,6 @@
 import click
 import sys
-from tabulate import tabulate
+import yaml
 
 from capstone import db, deployment
 
@@ -15,15 +15,10 @@ def site_option():
                         envvar="CAPSTONE_SITE",
                         required=True)
 
-def make_table(rows, **kwargs):
+def prettify(rows, **kwargs):
     # rows is a list of rows
     # each row is a dict with `"column_name": "value"` mapping
-    options = dict(
-        headers="keys",
-        tablefmt="grid",
-        maxcolwidths=12)
-    options.update(kwargs)
-    return tabulate(sanitize_rows(rows), **options)
+    return yaml.dump(rows, sort_keys=False)
 
 def sanitize_rows(rows):
     """Convert None values to str(None).
@@ -52,7 +47,7 @@ def sites_list():
     """Lists sites"""
     print("List sites")
     sites = db.Site.find_all()
-    print(make_table([site.get_dict() for site in sites]))
+    print(prettify([site.get_dict() for site in sites]))
 
 @sites.command("show")
 @click.argument("site")
@@ -65,9 +60,7 @@ def sites_show(site):
         sys.exit(1)
     else:
         site_dict = db_site.get_dict()
-        print(make_table(
-            {"key": list(site_dict.keys()), "value": list(site_dict.values())},
-            maxcolwidths=50))
+        print(prettify(site_dict))
 
 ## PROJECTS
 
@@ -86,7 +79,7 @@ def projects_list(site):
         print("Site not found")
         sys.exit(1)
     projects = db.Project.find_all(site_id=db_site.id)
-    print(make_table([project.get_teaser() for project in projects]))
+    print(prettify([project.get_teaser() for project in projects]))
 
 
 @projects.command("show")
@@ -106,13 +99,11 @@ def projects_show(site, project):
         sys.exit(1)
     project_dict = db_project.get_dict()
     project_dict["num_tasks"] = len(tasks)
-    print(make_table(
-        {"key": list(project_dict.keys()), "value": list(project_dict.values())},
-        maxcolwidths=60))
+    print(prettify(project_dict))
     if tasks:
         # TODO: maybe figure out a better way to print checks?
         print("Tasks")
-        print(make_table([
+        print(prettify([
             {
                 **t.get_dict(),
                 "checks": [c.title for c in t.get_checks()],
@@ -156,7 +147,7 @@ def users_list(site):
         print("Site not found")
         sys.exit(1)
     users = db.User.find_all(site_id=db_site.id)
-    print(make_table([user.get_teaser() for user in users]))
+    print(prettify([user.get_teaser() for user in users]))
 
 @users.command("show")
 @site_option()
@@ -173,10 +164,7 @@ def users_show(site, username):
         print("User not found")
         sys.exit(1)
     user_dict = db_user.get_dict()
-    print(make_table(
-        {"key": list(user_dict.keys()),
-         "value": list(user_dict.values())},
-        maxcolwidths=60))
+    print(prettify(user_dict))
 
 @users.command("delete")
 @site_option()
@@ -250,7 +238,7 @@ def user_projects_list(site, user, project):
     if not table_data:
         print("No matching rows found")
         sys.exit(0)
-    print(make_table(table_data))
+    print(prettify(table_data))
 
 @user_projects.command("show")
 @site_option()
@@ -268,10 +256,7 @@ def user_projects_show(site, user_project_id):
     user_project_dict = db_user_project.get_dict()
     project_name = db_user_project.get_project().name
     user_email = db_user_project.get_user().email
-    print(make_table(
-        {"key": list(user_project_dict.keys()) + ["project", "email"],
-         "value": list(user_project_dict.values()) + [project_name, user_email]},
-        maxcolwidths=60))
+    print(prettify(dict(user_project_dict, project=project_name, email=user_email)))
 
 @user_projects.command("delete")
 @site_option()
@@ -334,7 +319,7 @@ def deploys_list(site, user, project):
     if not deployments:
         print("No matching rows found")
         sys.exit(0)
-    print(make_table(deployments))
+    print(prettify(deployments))
 
 @deploys.command("new")
 @site_option()
