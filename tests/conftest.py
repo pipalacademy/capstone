8851691a6da9230e645d1a9b3dc514728ae87827
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-04-12 02:23:06 +0530

    modify tests for start_user_project to use a temporary gitto

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
+import os
+import platform
+import subprocess
+import time
+from contextlib import contextmanager
+from pathlib import Path
+
 import pytest
 
 from capstone.app import app as _app
@@ -9,6 +16,40 @@ def setup_db():
     """Fixture to start every test with a clean database.
     """
     db.db.query("TRUNCATE site CASCADE")
+
+@contextmanager
+def safe_popen(*args, **kwargs):
+    """Context manager that works exactly like subprocess.Popen, except that
+    the process is killed at the end of the with statement.
+        with safe_popen("python webapp.py 8080"):
+            time.sleep(0.5) # wait for the app to start
+            assert urlopen("http://localhost:8080/").code == 200
+        # The webserver will be automatically killed here
+    """
+    p = subprocess.Popen(*args, **kwargs)
+    try:
+        yield p
+    finally:
+        p.kill()
+
+def get_arch():
+    return platform.machine()
+
+@pytest.fixture()
+def gitto(tmp_path):
+    """Fixture to start a gitto server for testing."""
+    gitto_executable_path = Path(__file__).parent.parent / "gitto" / f"gitto-{get_arch()}"
+    with safe_popen(
+        [gitto_executable_path],
+        env={
+            "GITTO_ROOT": str(tmp_path),
+            "GITTO_PORT": "8080",
+            "GITTO_API_TOKEN": "gitto",
+            "PATH": os.getenv("PATH"),
+        }
+    ):
+        time.sleep(0.5)  # wait for the app to start
+        yield
 
 @pytest.fixture()
 def api_client():
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,58 +1,49 @@
-from unittest import mock
+import os
+import subprocess
+from pathlib import Path
 from unittest.mock import patch
 
-from capstone import config
-from capstone import db
+from capstone import config, db
+from capstone.utils import files
 from capstone.utils.user_project import delete_user_project, start_user_project
 
 from .test_db import site_id, project_id, user_id
 
 
-@patch("capstone.utils.user_project.extract_zipfile")
-@patch("capstone.utils.user_project.git")
-@patch("capstone.utils.user_project.gitto.create_repo", return_value="testid1234")
-@patch("capstone.utils.user_project.gitto.set_webhook")
-def test_start_user_project(
-        mock_set_webhook,
-        mock_create_repo,
-        mock_git,
-        mock_extract_zipfile,
-        project_id,
-        user_id):
-    project = db.Project.find(id=project_id)
-    user = db.User.find(id=user_id)
+def test_start_user_project(project_id, user_id, gitto, tmp_path):
+    project = db.Project.find_or_fail(id=project_id)
+    user = db.User.find_or_fail(id=user_id)
 
-    with patch("capstone.utils.user_project.gitto.get_repo",
-               return_value={"id": "testid1234",
-                             "git_url": f"{config.gitto_base_url}/testid1234/{project.name}",
-                             "name": project.name,
-                             "repo_name": f"testid1234/{project.name}"}):
-        user_project = start_user_project(project=project, user=user)
+    # setup repo.zip for project
+    with open(Path(tmp_path) / "test.txt", "w") as f:
+        f.write("test\n")
+    subprocess.check_call(["zip", "repo.zip", "test.txt"], cwd=tmp_path)
+    with open(Path(tmp_path) / "repo.zip", "rb") as f:
+        files.save_private_file(key=project.get_private_file_key_for_zipball(), stream=f)
 
-    assert user_project.project_id == project_id
-    assert user_project.user_id == user_id
-    assert user_project.git_url == f"{config.gitto_base_url}/testid1234/{project.name}"
-    assert user_project.repo_id == "testid1234"
+    user_project = start_user_project(project=project, user=user)
 
-    assert mock_extract_zipfile.called_once_with(
-        src=f"private/projects/{project.name}/repo-git.zip",
-        dst=mock.ANY
+    subprocess.check_call(
+        ["git", "clone", user_project.git_url], env={"PATH": os.getenv("PATH")}, cwd=tmp_path,
     )
-    assert mock_set_webhook.called_once_with(
-        id="testid1234",
-        webhook_url=f"{project.get_site().get_url()}/api/users/{user.username}/projects/{project.name}/hook/test1234",
-    )
+    repo_dir = Path(tmp_path) / project.name
+    assert repo_dir.is_dir()
+    assert {p.name for p in repo_dir.iterdir()} == {".git", "test.txt"}
+    with open(repo_dir / "test.txt") as f:
+        assert f.read() == "test\n"
 
 
 @patch("capstone.utils.user_project.gitto.delete_repo")
-@patch("capstone.utils.user_project.db.UserProject.delete")
-def test_delete_user_project(mock_db_delete, mock_delete_repo, project_id, user_id):
+def test_delete_user_project(mock_delete_repo, project_id, user_id):
     repo_name = "random_hash/project_name.git"
     git_url = f"{config.gitto_base_url}/{repo_name}"
     user_project = db.UserProject(
-        user_id=user_id, project_id=project_id, git_url=git_url, repo_id="random_hash").save()
+        user_id=user_id,
+        project_id=project_id,
+        git_url=git_url,
+        repo_id="random_hash"
+    ).save()
 
     delete_user_project(user_project=user_project)
 
-    assert mock_db_delete.called_once_with(user_project)
     assert mock_delete_repo.called_once_with(id="random_hash")
