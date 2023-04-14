74806e9c4131184822ac6fd3bfcfde2866696d87
Anand Chitipothu <anandology@gmail.com>
2023-04-14 18:59:08 +0530

    Added gitto as a nomad job

--- nomad/nomad.sh
+++ nomad/nomad.sh
@@ -3,4 +3,27 @@
 # Script to start Nomad 
 #
 
-exec sudo nomad agent -dev -bind 0.0.0.0 
+# Generates nomad.conf
+
+NOMAD_ROOT=`dirname $0`
+ROOT=`realpath $NOMAD_ROOT/..`
+echo $ROOT
+
+NOMAD_CONFIG=$ROOT/data/nomad.conf
+GIT_ROOT=$ROOT/data/git
+
+mkdir -p $ROOT/data/git
+
+echo "generating $NOMAD_CONFIG ..."
+echo "setting GIT_ROOT to $GIT_ROOT"
+
+cat > $NOMAD_CONFIG <<END
+client {
+  host_volume "git-volume" {
+    path      = "$GIT_ROOT"
+    read_only = false
+  }
+} 
+END
+
+exec sudo nomad agent -dev -bind 0.0.0.0  -config $NOMAD_CONFIG
