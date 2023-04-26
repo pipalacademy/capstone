a931b066ce0193a48be4ef4347a68c1c05920006
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-04-27 00:59:56 +0530

    bugfix: script to build docker image was deleting up-to-date image
    script to build and replace capstone-runner docker image was also
    deleting old images.
    the image ID for two identical images are the same. even if they it
    is built fresh. this case wasn't considered in the script, and it
    would delete the image with old ID.
    now it compares whether this old ID is different from new ID before
    deleting.

--- runner/build-and-replace-docker-image.sh
+++ runner/build-and-replace-docker-image.sh
@@ -7,14 +7,19 @@ image_tag="capstone-runner:latest"
 build_path="$(dirname $0)"
 
 # Check if an image with the same tag exists
-image_id=$(docker images -q "$image_tag")
+old_image_id=$(docker images -q "$image_tag")
 
 # Build the new Docker image
 echo "Building new Docker image with tag: $image_tag"
 docker build -t "$image_tag" "$build_path"
 
-# If an old version of the image exists, delete it
-if [ -n "$image_id" ]; then
-    echo "Deleting previous image with tag: $image_tag"
-    docker rmi -f "$image_id"
+# Delete previous version of image if it's not the same as new image
+image_id=$(docker images -q "$image_tag")
+if [ -z "$old_image_id" ]; then
+    echo "Previous version of image was not found"
+elif [ "$old_image_id" = "$image_id" ]; then
+    echo "Previous version of image is same as new version"
+else
+    echo "Deleting previous image with id: $old_image_id"
+    docker rmi -f "$old_image_id"
 fi
