c7b3526b48b5082f14ef1e8a4bfdbebd90220dde
Kaustubh M <37668193+nikochiko@users.noreply.github.com>
2023-05-03 22:35:25 +0000

    #57: Add tests for projects when working with tasks and checks
    This commit adds more tests for projects, when working with tasks
    and checks.

--- tests/integration/test_projects.py
+++ tests/integration/test_projects.py
@@ -10,6 +10,15 @@ from capstone.utils import git
 from .conftest import create_site, create_project
 
 
+example_check = {
+    "name": "check_http_request",
+    "title": "index page should return 200 OK",
+    "args": {
+        "endpoint": "/"
+    }
+}
+
+
 def test_cli_command_exits_with_zero_code():
     create_site(name="test", domain="test")
     result = subprocess.run([
@@ -87,7 +96,7 @@ def test_cli_command_publishes_the_project(capstone_app):
     assert project.is_published is True
 
 
-def test_metadata_is_updated_with_capstone_yml(capstone_app, tmp_path):
+def test_project_metadata_can_be_updated_by_author(capstone_app, tmp_path):
     site = create_site(name="localhost", domain="localhost")
     project = create_project(site, name="test-project", title="Test Project")
 
@@ -105,3 +114,129 @@ def test_metadata_is_updated_with_capstone_yml(capstone_app, tmp_path):
     project.refresh()
 
     assert project.title == "Updated title"
+
+
+def test_project_task_can_be_added_by_author(capstone_app, tmp_path):
+    site = create_site(name="localhost", domain="localhost")
+    project = create_project(site, name="test-project", title="Test Project")
+
+    repo = git.Repo.clone_from(project.git_url, tmp_path)
+    metadata_file = tmp_path / "capstone.yml"
+    example_task = dict(name="foo", title="Foo!", description="foo, not bar\n", checks=[])
+
+    metadata = yaml.safe_load(metadata_file.read_text())
+    metadata["tasks"].append(example_task)
+    metadata_file.write_text(yaml.safe_dump(metadata))
+
+    repo.add(metadata_file.name)
+    repo.commit(message="add task")
+    repo.push()
+
+    project.refresh()
+
+    tasks = project.get_tasks()
+    assert len(tasks) == len(metadata["tasks"])
+    assert tasks[-1].name == example_task["name"]
+    assert tasks[-1].title == example_task["title"]
+    assert tasks[-1].get_checks() == []
+
+
+def test_project_task_can_be_updated_by_author(capstone_app, tmp_path):
+    site = create_site(name="localhost", domain="localhost")
+    project = create_project(site, name="test-project", title="Test Project")
+
+    repo = git.Repo.clone_from(project.git_url, tmp_path)
+    metadata_file = tmp_path / "capstone.yml"
+    example_task = dict(name="foo", title="Foo!", description="foo, not bar\n", checks=[])
+
+    metadata = yaml.safe_load(metadata_file.read_text())
+    metadata["tasks"].append(example_task)
+    metadata_file.write_text(yaml.safe_dump(metadata))
+
+    repo.add(metadata_file.name)
+    repo.commit(message="add task")
+    repo.push()
+
+    # update title
+    metadata["tasks"][-1].update(title="New Foo!")
+    metadata_file.write_text(yaml.safe_dump(metadata))
+
+    repo.add(metadata_file.name)
+    repo.commit(message="update task")
+    repo.push()
+
+    project.refresh()
+    tasks = project.get_tasks()
+    assert len(tasks) == len(metadata["tasks"])
+    assert tasks[-1].name == example_task["name"]
+    assert tasks[-1].title == "New Foo!"
+
+
+def test_project_task_can_be_deleted_by_author(capstone_app, tmp_path):
+    site = create_site(name="localhost", domain="localhost")
+    project = create_project(site, name="test-project", title="Test Project")
+
+    repo = git.Repo.clone_from(project.git_url, tmp_path)
+    metadata_file = tmp_path / "capstone.yml"
+    example_task = dict(name="foo", title="Foo!", description="foo, not bar\n", checks=[dict(example_check)])
+
+    metadata = yaml.safe_load(metadata_file.read_text())
+    metadata["tasks"].append(example_task)
+    metadata_file.write_text(yaml.safe_dump(metadata))
+
+    repo.add(metadata_file.name)
+    repo.commit(message="add task")
+    repo.push()
+
+    project.refresh()
+    assert len(project.get_tasks()) == len(metadata["tasks"])
+
+    # delete task(s) -- including maybe any default tasks
+    metadata["tasks"] = []
+    metadata_file.write_text(yaml.safe_dump(metadata))
+
+    repo.add(metadata_file.name)
+    repo.commit(message="delete task")
+    repo.push()
+
+    project.refresh()
+    assert len(project.get_tasks()) == 0
+
+
+def test_project_tasks_can_be_reordered_by_author(capstone_app, tmp_path):
+    site = create_site(name="localhost", domain="localhost")
+    project = create_project(site, name="test-project", title="Test Project")
+
+    repo = git.Repo.clone_from(project.git_url, tmp_path)
+    metadata_file = tmp_path / "capstone.yml"
+    foo_task = dict(name="foo", title="Foo!", description="foo, not bar\n", checks=[dict(example_check)])
+    bar_task = dict(name="bar", title="Bar!", description="bar, not foo\n", checks=[dict(example_check)])
+
+    metadata = yaml.safe_load(metadata_file.read_text())
+    default_tasks = list(metadata["tasks"])
+    metadata["tasks"] = default_tasks + [foo_task, bar_task]
+    metadata_file.write_text(yaml.safe_dump(metadata))
+
+    repo.add(metadata_file.name)
+    repo.commit(message="add tasks")
+    repo.push()
+
+    # switch order, foo_task, bar_task -> bar_task, foo_task
+    metadata["tasks"] = default_tasks + [bar_task, foo_task]
+    metadata_file.write_text(yaml.safe_dump(metadata))
+
+    repo.add(metadata_file.name)
+    repo.commit(message="reorder tasks")
+    repo.push()
+
+    project.refresh()
+    tasks = project.get_tasks()
+    expected_bar_task, expected_foo_task = tasks[-2:]
+
+    assert expected_bar_task.name == "bar"
+    assert expected_bar_task.title == "Bar!"
+    assert len(expected_bar_task.get_checks()) == 1
+
+    assert expected_foo_task.name == "foo"
+    assert expected_foo_task.title == "Foo!"
+    assert len(expected_foo_task.get_checks()) == 1
